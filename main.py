3d335a11cfc9800c76d32b9878097993f6aa3146
jeremicaleksandar <jeremicaleksandar15@gmail.com>
2018-11-16 12:41:32 +0100

    sve u f-jama

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from scipy.integrate import odeint
 #import matplotlib.pyplot as plt
 #from matplotlib.patches import Circle
 
-# Pendulum rod lengths (m), bob masses (kg).
-L1, L2 = 1, 1
-m1, m2 = 1, 1
-# The gravitational acceleration (m.s-2).
 g = 9.81
+
+//https://scipython.com/blog/the-double-pendulum/
 
 def deriv(y, t, L1, L2, m1, m2):
     """Return the first derivatives of y = theta1, z1, theta2, z2."""
@@ -24,7 +22,7 @@ def deriv(y, t, L1, L2, m1, m2):
              m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
     return theta1dot, z1dot, theta2dot, z2dot
 
-def calc_E(y):
+def calc_E(y, m1, m2, L1, L2):
     """Return the total energy of the system."""
 
     th1, th1d, th2, th2d = y.T
@@ -33,35 +31,68 @@ def calc_E(y):
             2*L1*L2*th1d*th2d*np.cos(th1-th2))
     return T + V
 
-# Maximum time, time point spacings and the time grid (all in s).
-tmax, dt = 30, 0.01
-t = np.arange(0, tmax+dt, dt)
-# Initial conditions: theta1, dtheta1/dt, theta2, dtheta2/dt.
-y0 = np.array([3*np.pi/7, 0, 3*np.pi/4, 0])
 
-# Do the numerical integration of the equations of motion
-y = odeint(deriv, y0, t, args=(L1, L2, m1, m2))
+def solve(L1, L2, m1, m2, t, y0):
+	# Do the numerical integration of the equations of motion
+	y = odeint(deriv, y0, t, args=(L1, L2, m1, m2))
 
-# Check that the calculation conserves total energy to within some tolerance.
-EDRIFT = 0.05
-# Total energy from the initial conditions
-E = calc_E(y0)
-if np.max(np.sum(np.abs(calc_E(y) - E))) > EDRIFT:
-    sys.exit('Maximum energy drift of {} exceeded.'.format(EDRIFT))
+	# Check that the calculation conserves total energy to within some tolerance.
+	EDRIFT = 0.05
+	# Total energy from the initial conditions
+	E = calc_E(y0, m1, m2, L1, L2)
+	if np.max(np.sum(np.abs(calc_E(y, m1, m2, L1, L2) - E))) > EDRIFT:
+		sys.exit('Maximum energy drift of {} exceeded.'.format(EDRIFT))
 
-# Unpack z and theta as a function of time
-theta1, theta2 = y[:,0], y[:,2]
+	# Unpack z and theta as a function of time
+	theta1, theta2 = y[:,0], y[:,2]
 
-# Convert to Cartesian coordinates of the two bob positions.
-x1 = L1 * np.sin(theta1)
-y1 = -L1 * np.cos(theta1)
-x2 = x1 + L2 * np.sin(theta2)
-y2 = y1 - L2 * np.cos(theta2)
+	# Convert to Cartesian coordinates of the two bob positions.
+	x1 = L1 * np.sin(theta1)
+	y1 = -L1 * np.cos(theta1)
+	x2 = x1 + L2 * np.sin(theta2)
+	y2 = y1 - L2 * np.cos(theta2)
+	
+	return theta1, theta2, x1, y1, x2, y2
 
-from pprint import pprint
-pprint([theta1, theta2])
+def main():
 
-from matplotlib import pyplot as plt
-plt.plot(t, theta1, t, theta2)
-plt.savefig("seq.png")
+	# Pendulum rod lengths (m), bob masses (kg).
+	L1, L2 = 1, 1
+	m1, m2 = 1, 1
+	# The gravitational acceleration (m.s-2).
+
+
+	# Maximum time, time point spacings and the time grid (all in s).
+	tmax, dt = 30, 0.01
+	t = np.arange(0, tmax+dt, dt)
+	# Initial conditions: theta1, dtheta1/dt, theta2, dtheta2/dt.
+	y0 = np.array([3*np.pi/7, 0, 3*np.pi/4, 0])
+
+
+	theta1, theta2, x1, y1, x2, y2 = solve(L1, L2, m1, m2, t, y0)
+
+	from pprint import pprint
+	pprint([theta1, theta2])
+
+	plot_keys = [
+		'theta1',
+		'theta2',
+		#'x1',
+		#'y1',
+		#'x2',
+		#'y2',
+	]
+	from matplotlib import pyplot as plt
+	for key in plot_keys:
+		plt.plot(t, locals()[key], label=key)
+	#plt.plot(t, theta1, label='theta1')
+	#plt.plot(t, theta2, label='theta2')
+	plt.legend()
+	plt.savefig("seq.png")
+
+if __name__ == "__main__":
+	main()
+	
+
+
 
